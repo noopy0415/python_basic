1e3c3043307fd742d031e1efa48ab724e586de59
noopy0415 <noopy0415@gmail.com>
2018-11-21 21:40:15 +0900

    A-9終了

---
+++ A-9.py
@@ -0,0 +1,7 @@
+kazuma_info = {"first_name": "Kazuma",
+               "family_name": "Takahashi",
+               "age": 35}
+
+print(kazuma_info["first_name"])
+print(kazuma_info["family_name"])
+print(kazuma_info["age"])
